d045fcfb7d2a7ef16176d234e3d57bb510f7bf73
sjwakeman <swakeman@ca.rr.com>
2020-10-26 11:39:41 -0700

    Added data to seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+individual1 = Individual.create(name: 'individual one')
+individual2 = Individual.create(name: 'individual two')
+individual3 = Individual.create(name: 'individual three')
+ 
+Contact.create(name: 'contact one', date: 'March 1, 2020', category: 'work', location: 'office', occurrence: '10', individual_id: '1')
