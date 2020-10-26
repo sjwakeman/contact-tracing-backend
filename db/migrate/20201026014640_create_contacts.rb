3ba296256facf9e0f625e0a5c76d4fe1cd8b78c5
sjwakeman <swakeman@ca.rr.com>
2020-10-25 18:48:47 -0700

    rails g model Contact name date location occurrence individual_id:references

---
+++ db/migrate/20201026014640_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :date
+      t.string :location
+      t.string :occurrence
+      t.references :individual_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
