7014d72e9dd2144d5a002b88d1d21853e36ed504
sjwakeman <swakeman@ca.rr.com>
2020-10-27 17:48:24 -0700

    Added def create with strong params if save status accepted else contact errors full messages status unprocessable entity end end to contacts_controller.rb file.

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::ContactsController < ApplicationController
         render json: contacts
     end
 
+    # def create
+    #     contact = Contact.new(contact_params)
+    #     if contact.save
+    #         render json: contact, status: :accepted
+    #     else
+    #         render json: {errors: contact.errors.full_messages}, status: :unprocessible_entity
+    #     end
+    end
+
+    # private
+
+    # def contact_params
+    #     params.require(:contact).permit(:name, :date, :category, :location, :occurrence, :individual_id)
+    # end
 end
