a90a8a2552fb22a1b0bbc1ed724fb7777cadfdfb
sjwakeman <swakeman@ca.rr.com>
2020-10-28 08:44:35 -0700

    Added def index individuals = Individuals.all render json: IndividualSerializer.new(individuals) # render json: individuals end

--- app/controllers/api/v1/individuals_controller.rb
+++ app/controllers/api/v1/individuals_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::IndividualsController < ApplicationController
+    def index
+        individuals = Individual.all
+        
+        # render json: individuals
+        render json: IndividualSerializer.new(individuals)
+    end
 end
