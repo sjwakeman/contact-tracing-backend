7ad374c1d5c2c641926cffaeed24722163dc9146
sjwakeman <swakeman@ca.rr.com>
2020-10-28 16:36:14 -0700

    Added resources individuals only index to reoutes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :contacts, only: [:index, :create]
+      resources :individuals, only: [:index]
     end
   end
 end
