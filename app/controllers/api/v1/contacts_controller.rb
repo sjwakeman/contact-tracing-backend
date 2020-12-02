025a0683ea4018c5c2577df05673920d773823ef
sjwakeman <swakeman@ca.rr.com>
2020-12-02 10:37:35 -0800

    Change contact variable to instance variable @contact in update and find_contact methods in contacts_controller.rb file.

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ContactsController < ApplicationController
+    before_action :find_contact, only: [:update]
     def index
         contacts = Contact.all
 
@@ -25,9 +26,22 @@ class Api::V1::ContactsController < ApplicationController
         end
     end
 
+    def update
+        @contact.update(contact_params)
+        if @contact.save
+            render json: ContactSerializer.new(@contact), status: :accepted 
+        else
+            render json: {errors: @contact.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
     private
 
     def contact_params
         params.require(:contact).permit(:name, :date, :category, :location, :occurrence, :individual_id)
+    end
+
+    def find_contact
+        @contact = Contact.find(params[:id])
     end
 end
