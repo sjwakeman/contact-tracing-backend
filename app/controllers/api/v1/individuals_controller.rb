7c736488da0196b2448f731a0810e7826fa11f6e
sjwakeman <swakeman@ca.rr.com>
2021-01-22 17:39:35 -0800

    Copied over contacts_controller before_action, def create method, def update method private def individual_params private method and def find_individual private method to individuals_controller.rb file.

--- app/controllers/api/v1/individuals_controller.rb
+++ app/controllers/api/v1/individuals_controller.rb
@@ -1,8 +1,38 @@
 class Api::V1::IndividualsController < ApplicationController
+    before_action :find_individual, only: [:update]
     def index
         individuals = Individual.all
         
         # render json: individuals
         render json: IndividualSerializer.new(individuals)
     end
+
+    def create
+        individual = Individual.new(individual_params)
+        if individual.save
+            render json: IndividualSerializer.new(individual), status: :accepted
+        else
+            render json: {errors: individual.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+    
+    def update
+        @individual.update(individual_params)
+        if @individual.save
+            render json: IndividualSerializer.new(@individual), status: :accepted 
+        else
+            render json: {errors: @individual.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def individual_params
+        params.require(:individual).permit(:name, :contact_id)
+    end
+
+    def find_individual
+        @individual = Individual.find(params[:id])
+    end
+
 end
