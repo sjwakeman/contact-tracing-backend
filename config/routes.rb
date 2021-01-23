1154e5115e3d8bf0089ae284f5b66a45041b40a2
sjwakeman <swakeman@ca.rr.com>
2021-01-22 17:48:44 -0800

    Added :create, :update to config/routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :contacts, only: [:index, :create, :update]
-      resources :individuals, only: [:index]
+      resources :individuals, only: [:index, :create, :update]
     end
   end
 end
